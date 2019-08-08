aa52a1c5d8d6ebd117c33cacb9afaa53df91f4b1
Will <master_wu51@hotmail.com>
2019-08-08 16:33:24 +1000

    second commit

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,11 @@ export default class About extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="about" title="us" />
-            <div className="row" />
+            <div className="row">
+              <div className="col-10 mx-auto text-center text-title text-capitalize pt-5">
+                <p>[We Are Robots Online...]</p>
+              </div>
+            </div>
           </div>
         </div>
       </React.Fragment>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,11 @@ export default class Contact extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="contact" title="us" />
-            <div className="row" />
+            <div className="row">
+              <div className="col-10 mx-auto text-center text-title text-capitalize pt-5">
+                <p>[We Are ...]</p>
+              </div>
+            </div>
           </div>
         </div>
       </React.Fragment>
